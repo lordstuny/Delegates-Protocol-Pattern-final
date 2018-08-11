7ae05446a59dd2c1c5de5a56a620c69e0a3e0cdf
codesworth <codezworth@gmail.com>
2018-08-11 16:31:21 +0000

    Initial Commit

---
+++ Nerdy-Quiz-final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nerdy-Quiz-final
+//
+//  Created by Mensah Shadrach on 11/08/2018.
+//  Copyright © 2018 Mensah Shadrach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
